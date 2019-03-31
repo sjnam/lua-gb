ea819a46ac1605268e8e44b3625f7dbabdd7499f
sjnam <jsunam@gmail.com>
2019-03-31 20:24:32 +0900

    test_io

---
+++ test/test_io.lua
@@ -0,0 +1,74 @@
+local ffi = require "ffi"
+local gb_io = require "gb.io"
+local error = error
+
+
+if not gb_io.gb_open("test.dat") then
+    error("Can't open test.dat")
+end
+
+if gb_io.gb_number(10) ~= 123456789 then
+    error("fail to gb_number")
+end
+
+if gb_io.gb_digit(16) ~= 10 then
+    error("fail to gb_digit")
+end
+
+gb_io.gb_backup()
+gb_io.gb_backup()
+
+if gb_io.gb_number(16) ~= 0x9ABCDEF then
+    error("fail to gb_number")
+end
+
+if gb_io.gb_char() ~= '\n' then
+    error("fail to gb_char")
+end
+
+gb_io.gb_newline()
+
+if gb_io.gb_char() ~= '\n' then
+    error("fail to gb_char")
+end
+
+if gb_io.gb_char() ~= '\n' then
+    error("fail to gb_char")
+end
+
+if gb_io.gb_number(60) ~= 0 then
+    error("fail to gb_number")
+end
+
+local temp = ffi.new("char[?]", 100)
+gb_io.gb_string(temp, '\n')
+gb_io.gb_newline()
+gb_io.gb_string(temp, ':')
+
+if ffi.string(temp) ~= "Oops" then
+    error("fail to gb_string")
+end
+
+if gb_io.gb_digit(10) ~= -1 then
+    error("Digit error not detected")
+end
+
+if gb_io.gb_char() ~= ':' then
+    error("fail to gb_char")
+end
+
+if gb_io.gb_eof() then
+    error("premature eof")
+end
+
+gb_io.gb_newline()
+
+if not gb_io.gb_eof() then
+    error("fail to gb_eof")
+end
+
+if not gb_io.gb_close() then
+    error("fail to gb_close")
+end
+
+print("OK, the gb_io routines seem to work!");
